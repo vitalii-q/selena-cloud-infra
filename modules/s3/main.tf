cdf19f11decf350a473c2f6b5ef19407f8889a74
vitaly <vitalicheg28@mail.ru>
2025-09-04 13:41:21 +0200

    feat: autoloud .env files to s3 bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 
+  force_destroy = true # forced deletion even if files are present
+
   tags = var.tags
 }
 
@@ -10,4 +12,15 @@ resource "aws_s3_bucket_versioning" "this" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+# autoloud .env files
+resource "aws_s3_object" "env_files" {
+  for_each = fileset("${path.module}/../../environments/dev/s3_files", "*.env")
+
+  bucket = aws_s3_bucket.this.id
+  key    = each.value
+  source = "${path.module}/../../environments/dev/s3_files/${each.value}"
+
+  etag = filemd5("${path.module}/../../environments/dev/s3_files/${each.value}")
 }
