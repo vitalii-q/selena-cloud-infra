60c6a45f75e2e391fb2958e67eeb5c2d17f77c9f
vitaly <vitalicheg28@mail.ru>
2025-09-09 15:19:21 +0200

    refactor: outputs for vpc nets

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,14 +2,6 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id
-}
-
 output "default_security_group_id" {
   value = aws_vpc.main.default_security_group_id
 }
@@ -18,10 +10,31 @@ output "db_subnet_group" {
   value = aws_db_subnet_group.main.name
 }
 
+# nets
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
 output "public_subnet_2_id" {
   value = aws_subnet.public_subnet_2.id
 }
-
 output "private_subnet_2_id" {
   value = aws_subnet.private_subnet_2.id
+}
+
+# nets arrays
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_subnet.id,
+    aws_subnet.public_subnet_2.id
+  ]
+}
+output "private_subnet_ids" {
+  value = [
+    aws_subnet.private_subnet.id,
+    aws_subnet.private_subnet_2.id
+  ]
 }
