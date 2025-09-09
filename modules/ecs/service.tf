e0d360e9efb3915295a3cc8bf40a9cc615dd1d28
vitaly <vitalicheg28@mail.ru>
2025-09-09 14:27:09 +0200

    feat: add ecs task

---
+++ modules/ecs/service.tf
@@ -0,0 +1,21 @@
+resource "aws_ecs_service" "users_service" {
+  name            = "users-service"
+  cluster         = module.ecs_cluster.cluster_id
+  task_definition = aws_ecs_task_definition.users_service_task.arn
+  desired_count   = 1
+  launch_type     = "EC2"
+
+  network_configuration {
+    subnets          = [module.vpc.public_subnet_id]
+    security_groups  = [module.ec2.users_sg_id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = module.alb.users_service_tg_arn
+    container_name   = "users-service"
+    container_port   = 8080
+  }
+
+  depends_on = [module.alb.users_service_tg_arn]
+}
---
+++ modules/ecs/task.tf
@@ -0,0 +1,39 @@
+resource "aws_ecs_task_definition" "users_service_task" {
+  family                   = "users-service-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["EC2"]  # Для ASG. Если Fargate — ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = module.iam.ecs_task_execution_role_arn
+  task_role_arn            = module.iam.ecs_task_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "users-service"
+      image     = "${module.ecr.users_service_ecr_uri}:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
+      environment = [
+        {
+          name  = "DATABASE_URL"
+          value = "postgresql://${module.users_rds.username}:${data.aws_ssm_parameter.db_password.value}@${module.users_rds.endpoint}:5432/users_db"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = module.cloudwatch.logs_group_name
+          "awslogs-region"        = "eu-central-1"
+          "awslogs-stream-prefix" = "users-service"
+        }
+      }
+    }
+  ])
+}
