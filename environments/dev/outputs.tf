809de00ef7f19461c7351b8311c9ffd74d8b340a
vitaly <vitalicheg28@mail.ru>
2025-09-09 13:54:25 +0200

    fix: outputs

---
+++ environments/dev/outputs.tf
@@ -0,0 +1,3 @@
+output "users_service_ecr_uri" {
+  value = module.users.users_service_ecr_uri
+}
--- environments/dev/users/outputs.tf
+++ environments/dev/users/outputs.tf
@@ -9,3 +9,7 @@ output "users_alb_sg_id" {
 output "users_asg_name" {
   value = module.users_asg.asg_name
 }
+
+output "users_service_ecr_uri" {
+  value = module.ecr.users_service_ecr_uri
+}
