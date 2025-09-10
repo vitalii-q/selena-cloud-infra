a098ff88caa73515f50e9c445f015f739115bb05
vitaly <vitalicheg28@mail.ru>
2025-09-10 21:29:02 +0200

    feat: userdata for ecs install dependencies

--- scripts/userdata/userdata_ecs.sh
+++ scripts/userdata/userdata_ecs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
 # Подключаемся к ECS Cluster
-echo "ECS_CLUSTER=selena-users-cluster" >> /etc/ecs/ecs.config
-amazon-linux-extras install -y docker
-systemctl enable docker
-systemctl start docker
+echo "ECS_CLUSTER=selena-users-cluster" > /etc/ecs/ecs.config
+
+# Установка ECS Agent
+dnf install -y amazon-ecs-init
+systemctl enable --now ecs
+
+# Установка Docker
+amazon-linux-extras enable docker
+dnf install -y docker
+systemctl enable --now docker
