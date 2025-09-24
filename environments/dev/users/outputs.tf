f587063b2c7d54e0f7f0be6e2936231b58c2e5ef
vitaly <vitalicheg28@mail.ru>
2025-09-24 16:42:21 +0200

    refactor: delete alb resource in dev environment

--- environments/dev/users/outputs.tf
+++ environments/dev/users/outputs.tf
@@ -1,11 +1,3 @@
-output "users_service_alb_dns" {
-  value = aws_lb.users_service_alb.dns_name
-}
-
-output "users_alb_sg_id" {
-  value = aws_security_group.users_alb_sg.id
-}
-
 output "users_asg_name" {
   value = module.users_asg.asg_name
 }
