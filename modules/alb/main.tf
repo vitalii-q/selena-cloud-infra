f9e772511a4c15c719cc696255adc4a84cacdaaa
vitaly <vitalicheg28@mail.ru>
2025-10-26 15:51:16 +0100

    feat(ALB): add Target Group for ALB

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -14,3 +14,24 @@ resource "aws_lb" "users_alb" {
     Environment = "dev"
   }
 }
+
+resource "aws_lb_target_group" "users_tg" {
+  name     = "${var.name}-tg"
+  port     = var.target_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = var.health_check
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+
+  tags = {
+    Name = "${var.name}-tg"
+    Environment = var.environment
+  }
+}
--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -5,3 +5,7 @@ output "alb_arn" {
 output "alb_dns_name" {
   value = aws_lb.users_alb.dns_name
 }
+
+output "target_group_arn" {
+  value = aws_lb_target_group.users_tg.arn
+}
