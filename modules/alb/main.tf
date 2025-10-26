98308a7aa866608a99bf93d645678eadc07ef35e
vitaly <vitalicheg28@mail.ru>
2025-10-26 16:02:17 +0100

    feat(ALB): add HTTP Listener for ALB

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -35,3 +35,16 @@ resource "aws_lb_target_group" "users_tg" {
     Environment = var.environment
   }
 }
+
+resource "aws_lb_listener" "users_http_listener" {
+  load_balancer_arn = aws_lb.users_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.users_tg.arn
+  }
+
+  depends_on = [aws_lb.users_alb, aws_lb_target_group.users_tg] # guarantee that Listener is created after ALB and Target Group
+}
