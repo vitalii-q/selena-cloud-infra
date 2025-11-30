6eabcad7bc8a74ad5df4c7f15b852975356aed07
vitaly <vitalicheg28@mail.ru>
2025-11-30 10:06:49 +0100

    feat(iam user): added user iam for access to AWS ECR from CI/CD (GitHub Actions)

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -14,3 +14,13 @@ output "ecs_task_role_arn" {
   value = aws_iam_role.ecs_task_role.arn
 }
 
+# AWS_ACCESS_KEY_ID for ci/cd access
+output "github_actions_access_key_id" {
+  value = aws_iam_access_key.github_actions_key.id
+}
+
+# AWS_SECRET_ACCESS_KEY for ci/cd access
+output "github_actions_secret_access_key" {
+  value     = aws_iam_access_key.github_actions_key.secret
+  sensitive = true
+}
