e116287f65a5afe3783f22fd5edfd7919a7d324e
vitaly <vitalicheg28@mail.ru>
2025-12-19 14:00:13 +0100

    refactor(iam): root IAM to the separate file

---
+++ terraform/environments/dev/root_iam.tf
@@ -0,0 +1,48 @@
+module "shared_policies" {
+  source     = "../../modules/iam/iam-policies/shared"
+  account_id = var.account_id
+  region     = var.region
+}
+
+module "selena_cloudwatch_role" {
+  source        = "../../modules/iam/iam-roles/cloudwatch-role"
+  role_name     = "selena-cloudwatch-role"
+  service       = "ec2.amazonaws.com"
+
+  policies = {
+    CloudWatchMetricsPolicy = module.shared_policies.cloudwatch_metrics_policy_arn # customer managed policies
+  }
+
+  tags = {
+    Project = "Selena"
+  }
+}
+
+module "github_actions_role" {   # TODO: it may need to be moved to a higher level or duplicated in hotels infrastructure
+  source        = "../../modules/iam/iam-roles/github-actions-role"
+  role_name     = "selena-github-actions-role"
+  service       = "ec2.amazonaws.com"
+
+  policies = {
+    GitHubActionsECRPolicy = module.shared_policies.github_actions_ecr_policy_arn
+  }
+
+  tags = {
+    Project = "Selena"
+    Service = "CI/CD"
+  }
+}
+
+module "instance_management_role" {
+  source        = "../../modules/iam/iam-roles/service-role"
+  role_name     = "selena-instance-management-role"
+  service       = "ec2.amazonaws.com"
+
+  policies = {
+    Ec2StopStartPolicy = module.shared_policies.ec2_stop_start_policy_arn
+  }
+
+  tags = {
+    Project = "Selena"
+  }
+}
